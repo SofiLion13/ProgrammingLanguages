5d7501e6b2f4c858992bdf9e0de67f80ab19ab6b
Sofia <dragsy13@bk.ru>
2023-01-28 22:51:49 +0300

    Add new programm

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
